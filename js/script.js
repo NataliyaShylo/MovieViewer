a028870b6c89aa6207415a7d67a162ce363efb11
NataliyaShylo <nataliyashylo@gmail.com>
2021-11-01 09:29:52 +0200

    Practice 2. Added loop and conditions

--- js/script.js
+++ js/script.js
@@ -8,12 +8,25 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const movie1 = prompt("What was the fast film you have seen?", ""),
-      rate1 = prompt("How are you rate it?", ""),
-      movie2 = prompt("What was the fast film you have seen?", ""),
-      rate2 = prompt("How are you rate it?", "");
-
-personalMovieDB.movies[movie1] = rate1;
-personalMovieDB.movies[movie2] = rate2;
-
+let movie, rate;
+for (let i = 0; i < 2; i++){
+        movie = prompt("What was the last film you have seen?", "");
+        rate = prompt("How are you rate it?", "");
+        if (movie != "" && rate != "" && movie != null && rate != null && movie.length <= 50) {
+            personalMovieDB.movies[movie] = rate;
+            console.log("Save the answer");
+        } else {
+            console.log("Error. The entered data not valid");
+            i--;
+        }
+}
+if (personalMovieDB.count < 10) {
+    console.log("You have seen too few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classical viewer");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You are a movie fan");
+} else {
+    console.log("A mistake happens");
+}
 console.log(personalMovieDB);
